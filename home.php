c3e13d43c8085657d05ee13098b7bcce7956eb93
John Costallos <jpcostallos@gmail.com>
2014-06-17 14:18:06 -0400

    Create events and home page template. Home will have most recent featured event

---
+++ home.php
@@ -0,0 +1,66 @@
+<?php /* Template Name: Home */ ?>
+
+<?php get_header(); ?>
+
+			<div class="content-area">
+				<div class="main"> 
+					<?php
+					while (have_posts()) {
+						the_post();
+						get_template_part( 'content', 'page' );
+					} ?>
+				</div>
+					<section class="events">
+					<h1>Next Upcoming Event</h1>
+
+<?php
+						$counter = 0;
+						$eventLoop = new WP_QUERY(array('post_type' => 'osi-events', 'posts_per_page' => 1, 'orderby' =>'meta_value', 'order' => 'ASC', 'meta_key' => 'oe-form-start', 'meta_value' => time(), 'meta_compare' => '>='));
+						while ($eventLoop->have_posts()) {
+							$eventLoop->the_post();
+							$title = get_the_title();
+							$start = get_post_meta($post->ID, 'oe-form-start', true);
+							$end = get_post_meta($post->ID, 'oe-form-end', true);
+							$content = get_the_content();
+							$link = get_post_meta($post->ID, 'oe-form-url', true);
+							$image = get_the_post_thumbnail($post->ID, 'medium');
+							$image_url = wp_get_attachment_image_src(get_post_thumbnail_id( $post->ID ), 'full');
+
+							if ($end == "none")
+								$dates = date('l F jS, g:ia', $start);
+							else if (date('F j', $start) == date('F j', $end))
+								$dates = date('l F jS, g:ia', $start) . " - " . date('g:ia', $end);
+							else
+								$dates = date('F jS, g:ia', $start) . " to " . date('F jS, g:ia', $end);
+?>	
+						<article class="event">
+							<a class="img fancybox" href="<?php echo $image_url[0]; ?>"><?php echo $image; ?></a>
+							<h3><?php echo $title; ?></h3>
+							<h4><?php echo $dates; ?></h4>
+							<p><?php echo $content; ?></p>
+<?php
+							if ($link != "") {
+?>
+							<p><a href="<?php echo $link; ?>" target="_blank">Purchase Tickets HERE</a></p>
+<?php				}
+?>
+						</article>
+<?php
+						$counter++;
+					}
+
+					if ($counter == 0) {
+?>
+						<p>There are no events to display... yet.</p>
+	<?php
+					}
+	?>
+
+						<p><a href="<?php echo site_url('/events/'); ?>">See More Events</a></p>
+					</section>
+			</div>
+
+<?php get_footer(); ?>
+
+
+
