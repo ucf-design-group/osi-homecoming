198431ecb7f9f6e1350424bdb7c96ce442231580
chris-judge <Chris_Judge941@yahoo.com>
2014-07-30 18:42:19 -0400

    Added Font Icons.

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
 		<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 		<![endif]-->
 		<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 	</head>
 <?php
 	global $post;
