e7de82f1163ef6677b664e3cc2a04dee2af24128
John Costallos <jpcostallos@gmail.com>
2014-08-18 16:22:21 -0400

    add sponsors table and icons, also remove unused pattern

--- sponsors.php
+++ sponsors.php
@@ -29,10 +29,10 @@
 					</section>
 					<section class="sponsor-table">
 						<div class="table-holder">
-							<div id="firstcol"><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/friend-of-logo.png" alt="Friend of Homecoming!" /></div>	
-							<div id="seccol"><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/black-knight-logo.png" alt="Black Knight!" /></div>	
-							<div id="thirdcol"><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/gold-knight-logo.png" alt="Gold Knight!" /></div>
-							<div id="fourthcol"><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/ultimate-knight-logo.png" alt="Ultimate Knight!" /></div>
+							<div id="firstcol"><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/friend-of-logo.png" alt="Friend of Homecoming!" /></div>	
+							<div id="seccol"><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/black-knight-logo.png" alt="Black Knight!" /></div>	
+							<div id="thirdcol"><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/gold-knight-logo.png" alt="Gold Knight!" /></div>
+							<div id="fourthcol"><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/ultimate-knight-logo.png" alt="Ultimate Knight!" /></div>
 							<br />
 							<table>
 								<tr class="shader">
@@ -51,77 +51,78 @@
 								</tr>
 								<tr class="shader">
 									<td class="wide-cell"><p>Name/Logo on Homecoming website/Facebook <br />(over 10,000 followers)</p></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/friend.png" alt="Friend of Homecoming" /></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/black-knight.png" alt="Black Knight" /></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/gold-knight.png" alt="Gold Knight" /></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/ultimate-knight.png" alt="Ultimate Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/friend.png" alt="Friend of Homecoming" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/black-knight.png" alt="Black Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/gold-knight.png" alt="Gold Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/ultimate-knight.png" alt="Ultimate Knight" /></td>
 								</tr>
 								<tr>
 									<td class="wide-cell"><p>Company banner displayed at event(s)*</p></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/friend.png" alt="Friend of Homecoming" /></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/black-knight.png" alt="Black Knight" /></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/gold-knight.png" alt="Gold Knight" /></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/ultimate-knight.png" alt="Ultimate Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/friend.png" alt="Friend of Homecoming" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/black-knight.png" alt="Black Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/gold-knight.png" alt="Gold Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/ultimate-knight.png" alt="Ultimate Knight" /></td>
 								</tr>
 								<tr class="shader">
 									<td class="wide-cell"><p>Name/logo displayed on video screens/announced during event(s)</p></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/friend.png" alt="Friend of Homecoming" /></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/black-knight.png" alt="Black Knight" /></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/gold-knight.png" alt="Gold Knight" /></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/ultimate-knight.png" alt="Ultimate Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/friend.png" alt="Friend of Homecoming" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/black-knight.png" alt="Black Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/gold-knight.png" alt="Gold Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/ultimate-knight.png" alt="Ultimate Knight" /></td>
 								</tr>
 								<!-- <tr>
 									<td class="wide-cell"><p>Complimentary entry into the Homecoming parade <br />($50 value)</p></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/friend.png" alt="Friend of Homecoming" /></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/black-knight.png" alt="Black Knight" /></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/gold-knight.png" alt="Gold Knight" /></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/ultimate-knight.png" alt="Ultimate Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/friend.png" alt="Friend of Homecoming" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/black-knight.png" alt="Black Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/gold-knight.png" alt="Gold Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/ultimate-knight.png" alt="Ultimate Knight" /></td>
 								</tr> -->
 								<tr>
 									<td class="wide-cell"><p>Name/logo displayed on event(s) print advertising</p></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/friend.png" alt="Friend of Homecoming" /></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/black-knight.png" alt="Black Knight" /></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/gold-knight.png" alt="Gold Knight" /></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/ultimate-knight.png" alt="Ultimate Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/friend.png" alt="Friend of Homecoming" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/black-knight.png" alt="Black Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/gold-knight.png" alt="Gold Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/ultimate-knight.png" alt="Ultimate Knight" /></td>
 								</tr>
 								<tr class="shader">
 									<td class="wide-cell"><p>Name/logo on Spirit Splash shirt <br />(approx 5,000 produced)</p></td>
 									<td></td>
 									<td></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/gold-knight.png" alt="Gold Knight" /></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/ultimate-knight.png" alt="Ultimate Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/gold-knight.png" alt="Gold Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/ultimate-knight.png" alt="Ultimate Knight" /></td>
 								</tr>
 								<tr>
 									<td class="wide-cell"><p>Name/logo on official Homecoming shirt <br />(approx 5,000 produced)</p></td>
 									<td></td>
 									<td></td>
 									<td></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/ultimate-knight.png" alt="Ultimate Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/ultimate-knight.png" alt="Ultimate Knight" /></td>
 								</tr>
 								<tr class="shader">
 									<td class="wide-cell"><p>Promotional video to play at event(s)**</p></td>
 									<td></td>
 									<td></td>
 									<td></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/ultimate-knight.png" alt="Ultimate Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/ultimate-knight.png" alt="Ultimate Knight" /></td>
 								</tr>
 								<tr>
 									<td class="wide-cell"><p>Name/logo on promotional items***</p></td>
 									<td></td>
 									<td></td>
 									<td></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/ultimate-knight.png" alt="Ultimate Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/ultimate-knight.png" alt="Ultimate Knight" /></td>
 								</tr>
 								<tr class="shader">
 									<td class="wide-cell"><p>Oppurtunity to reach nearly 60,000 students at the second largest university in the nation!</p></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/friend.png" alt="Friend of Homecoming" /></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/black-knight.png" alt="Black Knight" /></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/gold-knight.png" alt="Gold Knight" /></td>
-									<td><img src="http://osi.ucf.edu/homecoming/wp-content/themes/homecoming/images/ultimate-knight.png" alt="Ultimate Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/friend.png" alt="Friend of Homecoming" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/black-knight.png" alt="Black Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/gold-knight.png" alt="Gold Knight" /></td>
+									<td><img src="<?php echo get_stylesheet_directory_uri(); ?>/resources/table/ultimate-knight.png" alt="Ultimate Knight" /></td>
 								</tr>
 							</table>
 						</div>
-						<sub>* Business must provide banner.
+						<sub>
+							* Business must provide banner.
 							<br />** Business must create video.
 							<br />*** Promotional items allotted where space allowed.
 						</sub>
