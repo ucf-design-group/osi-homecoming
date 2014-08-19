719a7f79702362609b33da64d7081d1b080ce92e
chris-judge <chris_judge941@yahoo.com>
2014-08-19 11:52:28 -0400

    Restructure volunteer php.

--- volunteer.php
+++ volunteer.php
@@ -4,16 +4,18 @@ get_header(); ?>
 
 			<div class="content-area">
 				<div class="main">
-					<h1>KNIGHTRO WANTS YOU</h1>
 					<div class="imgWrap">
 						<img class="historyImg" src="<?php echo get_stylesheet_directory_uri(); ?>/resources/volunteer.png" alt="History Page Image">					
 					</div>
-					<h2>To Volunteer For Homecoming.</h2>
+					<div class="descriptionWrap">
+						<h1>KNIGHTRO WANTS YOU</h1>
+						<h2>To Volunteer For Homecoming.</h2>
 					<?php
 					while (have_posts()) {
 						the_post();
 						get_template_part( 'content' );
 					} ?>
+					</div>
 				</div>
 				<!-- <aside> -->
 					<!-- OPTIONAL -->
