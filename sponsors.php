0ef3de69166664b0b468f4f4e0fbd205af480c30
chris-judge <chris_judge941@yahoo.com>
2014-08-19 11:51:18 -0400

    Remove title from sponsorship php.

--- sponsors.php
+++ sponsors.php
@@ -7,7 +7,7 @@
 					<?php
 					while (have_posts()) {
 						the_post();
-						get_template_part( 'content', 'page' );
+						get_template_part( 'content');
 					} ?>
 				
 					<section class="sponsors">
